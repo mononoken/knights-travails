a93e40d93884650fa4227e458648363d9c70761b
mononoken <ken.maeshima@pm.me>
2022-07-07 20:33:54 -0700

    Rename data to position

--- lib/node.rb
+++ lib/node.rb
@@ -2,12 +2,12 @@
 
 # Node for use in a binary search tree (BST)
 class Node
-  attr_accessor :data, :children
+  attr_accessor :position, :children
 
   # How to make sure argument is a specific type?
   # data is an instance of Position
-  def initialize(data = nil, children = [])
-    @data = data
+  def initialize(position = nil, children = [])
+    @position = position
     @children = children
   end
 
